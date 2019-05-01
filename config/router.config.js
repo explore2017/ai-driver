88ca7bc79f324e6f725a661de7e20654f7da6321
WORXFR <3475296507@qq.com>
2019-05-01 15:49:48 +0800

    first commit

---
+++ config/router.config.js
@@ -0,0 +1,363 @@
+export default [
+  // user
+  {
+    path: '/user',
+    component: '../layouts/UserLayout',
+    routes: [
+      { path: '/user', redirect: '/user/login' },
+      { path: '/user/login', name: 'login', component: './User/Login' },
+      { path: '/user/register', name: 'register', component: './User/Register' },
+      {
+        path: '/user/register-result',
+        name: 'register.result',
+        component: './User/RegisterResult',
+      },
+      {
+        component: '404',
+      },
+    ],
+  },
+  // app
+  {
+    path: '/',
+    component: '../layouts/BasicLayout',
+    Routes: ['src/pages/Authorized'],
+    routes: [
+
+
+      
+      // student
+      { path: '/', redirect: '/student/studentData', authority: ['admin', 'user'] },
+      {
+        path: '/student',
+        name: '学生',
+        icon: 'dashboard',
+        routes: [
+          {
+            path: '/student/studentData',
+            name: '个人信息',
+            component: './Student/StudentData',
+          },
+          {
+            path: '/student/choiceCoach',
+            name: '选择教练',
+            component: './Student/ChoiceCoach',
+          },
+          {
+            path: '/student/addExam',
+            name: '报名考试',
+            component: './Student/AddExam',
+          },
+          {
+            path: '/student/searchExam',
+            name: '查看考试',
+            component: './Student/SearchExam',
+          },
+        ],
+      },
+      // manage
+      { path: '/', redirect: '/manage/studentData', authority: ['admin', 'user'] },
+      {
+        path: '/manage',
+        name: '管理',
+        icon: 'dashboard',
+        routes: [
+          {
+            path: '/manage/student',
+            name: '学生管理',
+            component: './Manage/Student',
+          },
+          // {
+          //   path: '/manage/coach',
+          //   name: '教练管理',
+          //   component: './Manage/Coach',
+          // },
+          {
+            path: '/manage/exam',
+            name: '考试管理',
+            component: './Manage/Exam',
+          },
+          {
+            path: '/manage/vehicle',
+            name: '车辆管理',
+            component: './Manage/Vehicle',
+          },
+        ],
+      },
+
+
+
+
+      // dashboard
+      { path: '/', redirect: '/dashboard/analysis', authority: ['admin', 'user'] },
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        icon: 'dashboard',
+        routes: [
+          {
+            path: '/dashboard/analysis',
+            name: 'analysis',
+            component: './Dashboard/Analysis',
+          },
+          {
+            path: '/dashboard/monitor',
+            name: 'monitor',
+            component: './Dashboard/Monitor',
+          },
+          {
+            path: '/dashboard/workplace',
+            name: 'workplace',
+            component: './Dashboard/Workplace',
+          },
+        ],
+      },
+      // forms
+      {
+        path: '/form',
+        icon: 'form',
+        name: 'form',
+        routes: [
+          {
+            path: '/form/basic-form',
+            name: 'basicform',
+            component: './Forms/BasicForm',
+          },
+          {
+            path: '/form/step-form',
+            name: 'stepform',
+            component: './Forms/StepForm',
+            hideChildrenInMenu: true,
+            routes: [
+              {
+                path: '/form/step-form',
+                redirect: '/form/step-form/info',
+              },
+              {
+                path: '/form/step-form/info',
+                name: 'info',
+                component: './Forms/StepForm/Step1',
+              },
+              {
+                path: '/form/step-form/confirm',
+                name: 'confirm',
+                component: './Forms/StepForm/Step2',
+              },
+              {
+                path: '/form/step-form/result',
+                name: 'result',
+                component: './Forms/StepForm/Step3',
+              },
+            ],
+          },
+          {
+            path: '/form/advanced-form',
+            name: 'advancedform',
+            authority: ['admin'],
+            component: './Forms/AdvancedForm',
+          },
+        ],
+      },
+      // list
+      {
+        path: '/list',
+        icon: 'table',
+        name: 'list',
+        routes: [
+          {
+            path: '/list/table-list',
+            name: 'searchtable',
+            component: './List/TableList',
+          },
+          {
+            path: '/list/basic-list',
+            name: 'basiclist',
+            component: './List/BasicList',
+          },
+          {
+            path: '/list/card-list',
+            name: 'cardlist',
+            component: './List/CardList',
+          },
+          {
+            path: '/list/search',
+            name: 'searchlist',
+            component: './List/List',
+            routes: [
+              {
+                path: '/list/search',
+                redirect: '/list/search/articles',
+              },
+              {
+                path: '/list/search/articles',
+                name: 'articles',
+                component: './List/Articles',
+              },
+              {
+                path: '/list/search/projects',
+                name: 'projects',
+                component: './List/Projects',
+              },
+              {
+                path: '/list/search/applications',
+                name: 'applications',
+                component: './List/Applications',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        icon: 'profile',
+        routes: [
+          // profile
+          {
+            path: '/profile/basic',
+            name: 'basic',
+            component: './Profile/BasicProfile',
+          },
+          {
+            path: '/profile/basic/:id',
+            name: 'basic',
+            hideInMenu: true,
+            component: './Profile/BasicProfile',
+          },
+          {
+            path: '/profile/advanced',
+            name: 'advanced',
+            authority: ['admin'],
+            component: './Profile/AdvancedProfile',
+          },
+        ],
+      },
+      {
+        name: 'result',
+        icon: 'check-circle-o',
+        path: '/result',
+        routes: [
+          // result
+          {
+            path: '/result/success',
+            name: 'success',
+            component: './Result/Success',
+          },
+          { path: '/result/fail', name: 'fail', component: './Result/Error' },
+        ],
+      },
+      {
+        name: 'exception',
+        icon: 'warning',
+        path: '/exception',
+        routes: [
+          // exception
+          {
+            path: '/exception/403',
+            name: 'not-permission',
+            component: './Exception/403',
+          },
+          {
+            path: '/exception/404',
+            name: 'not-find',
+            component: './Exception/404',
+          },
+          {
+            path: '/exception/500',
+            name: 'server-error',
+            component: './Exception/500',
+          },
+          {
+            path: '/exception/trigger',
+            name: 'trigger',
+            hideInMenu: true,
+            component: './Exception/TriggerException',
+          },
+        ],
+      },
+      {
+        name: 'account',
+        icon: 'user',
+        path: '/account',
+        routes: [
+          {
+            path: '/account/center',
+            name: 'center',
+            component: './Account/Center/Center',
+            routes: [
+              {
+                path: '/account/center',
+                redirect: '/account/center/articles',
+              },
+              {
+                path: '/account/center/articles',
+                component: './Account/Center/Articles',
+              },
+              {
+                path: '/account/center/applications',
+                component: './Account/Center/Applications',
+              },
+              {
+                path: '/account/center/projects',
+                component: './Account/Center/Projects',
+              },
+            ],
+          },
+          {
+            path: '/account/settings',
+            name: 'settings',
+            component: './Account/Settings/Info',
+            routes: [
+              {
+                path: '/account/settings',
+                redirect: '/account/settings/base',
+              },
+              {
+                path: '/account/settings/base',
+                component: './Account/Settings/BaseView',
+              },
+              {
+                path: '/account/settings/security',
+                component: './Account/Settings/SecurityView',
+              },
+              {
+                path: '/account/settings/binding',
+                component: './Account/Settings/BindingView',
+              },
+              {
+                path: '/account/settings/notification',
+                component: './Account/Settings/NotificationView',
+              },
+            ],
+          },
+        ],
+      },
+      //  editor
+      {
+        name: 'editor',
+        icon: 'highlight',
+        path: '/editor',
+        routes: [
+          {
+            path: '/editor/flow',
+            name: 'flow',
+            component: './Editor/GGEditor/Flow',
+          },
+          {
+            path: '/editor/mind',
+            name: 'mind',
+            component: './Editor/GGEditor/Mind',
+          },
+          {
+            path: '/editor/koni',
+            name: 'koni',
+            component: './Editor/GGEditor/Koni',
+          },
+        ],
+      },
+      {
+        component: '404',
+      },
+    ],
+  },
+];
---
+++ src/pages/Student/ChoiceCoach.js
@@ -0,0 +1,151 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import { formatMessage, FormattedMessage } from 'umi-plugin-react/locale';
+import {
+  Form,
+  Input,
+  DatePicker,
+  Select,
+  Button,
+  Card,
+  InputNumber,
+  Radio,
+  Icon,
+  Tooltip,
+} from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import styles from './ChoiceCoach.less';
+
+const FormItem = Form.Item;
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+@connect(({ loading }) => ({
+  submitting: loading.effects['form/submitRegularForm'],
+}))
+@Form.create()
+class ChoiceCoach extends PureComponent {
+  handleSubmit = e => {
+    const { dispatch, form } = this.props;
+    e.preventDefault();
+    form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        dispatch({
+          type: 'form/submitRegularForm',
+          payload: values,
+        });
+      }
+    });
+  };
+
+  render() {
+    const { submitting } = this.props;
+    const {
+      form: { getFieldDecorator, getFieldValue },
+    } = this.props;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 7 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+        md: { span: 10 },
+      },
+    };
+
+    const submitFormLayout = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 10, offset: 7 },
+      },
+    };
+
+    return (
+      <PageHeaderWrapper
+        title={<FormattedMessage id="app.forms.student.title" />}
+        content={<FormattedMessage id="app.forms.student.description" />}
+      >
+        <Card bordered={false}>
+          <Form onSubmit={this.handleSubmit} hideRequiredMark style={{ marginTop: 8 }}>
+            <FormItem {...formItemLayout} label={<FormattedMessage id="form.student.choiceCampus" />}>
+              {getFieldDecorator('campus', {
+                rules: [
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'validation.title.required' }),
+                  },
+                ],
+              })(
+                // <Input placeholder={formatMessage({ id: 'form.student.placeholder' })} />
+                <Select defaultValue="2" style={{ width: 120 }}>
+                  <Option value="1" disabled>科目一</Option>
+                  <Option value="2">科目二</Option>
+                  <Option value="3">科目三</Option>
+                  <Option value="4" disabled>科目四</Option>
+                </Select>
+              )}
+            </FormItem>
+            <FormItem {...formItemLayout} label={<FormattedMessage id="form.student.choiceCoach" />}>
+              {getFieldDecorator('coach', {
+                rules: [
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'validation.title.required' }),
+                  },
+                ],
+              })(
+                // <Input placeholder={formatMessage({ id: 'form.student.placeholder' })} />
+                <Select defaultValue="2" style={{ width: 120 }}>
+                  <Option value="1" disabled>花江</Option>
+                  <Option value="2">金鸡岭</Option>
+                  <Option value="3">象鼻山</Option>
+                </Select>
+              )}
+            </FormItem>
+            <FormItem {...formItemLayout} label={<FormattedMessage id="form.student.price" />}>
+              {getFieldDecorator('price', {
+                rules: [
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'validation.title.required' }),
+                  },
+                ],
+              })(
+                // <Input placeholder={formatMessage({ id: 'form.student.placeholder' })} />
+                <Select defaultValue="1" style={{ width: 120 }}>
+                  <Option value="1" disabled>1000</Option>
+                </Select>
+              )}
+            </FormItem>
+            <FormItem {...formItemLayout} label={<FormattedMessage id="form.student.phone.label" />}>
+              {getFieldDecorator('phone', {
+                rules: [
+                  {
+                    required: true,
+                    message: formatMessage({ id: 'validation.title.required' }),
+                  },
+                ],
+              })(
+                <Input placeholder={formatMessage({ id: 'form.student.phone.placeholder' })} />
+              )}
+            </FormItem>
+            <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+              <Button type="primary" htmlType="submit" loading={submitting}>
+                <FormattedMessage id="form.submit" />
+              </Button>
+              <Button style={{ marginLeft: 8 }}>
+                <FormattedMessage id="form.save" />
+              </Button>
+            </FormItem>
+          </Form>
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default ChoiceCoach;
